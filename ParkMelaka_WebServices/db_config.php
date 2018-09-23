4db80403017fa78dccc24b7222c8fa2035e20975
KS Koh <kahsiang1123@outlook.com>
2018-09-23 21:11:05 +0800

    fix(database): correct the sql_scrum file

--- ParkMelaka_WebServices/db_config.php
+++ ParkMelaka_WebServices/db_config.php
@@ -6,13 +6,11 @@
  * Time: 11:45 AM
  */
 
-/**define('DB_USER', "root");
-*define('DB_PASSWORD', "root");
-*define('DB_DATABASE', "parkmelaka");
-*define('DB_SERVER', "localhost");
-**/
-define('DB_USER', "testAdmin");
-define('DB_PASSWORD', "123456");
+define('DB_USER', "root");
+define('DB_PASSWORD', "root");
+// define('DB_USER', "testAdmin");
+// define('DB_PASSWORD', "123456");
+
 define('DB_DATABASE', "parkmelaka");
 define('DB_SERVER', "localhost");
 ?>
--- ParkMelaka_WebServices/sql_scrum/top2121_sqlscrum.sql
+++ ParkMelaka_WebServices/sql_scrum/top2121_sqlscrum.sql
@@ -13,12 +13,11 @@ CREATE TABLE IF NOT EXISTS users(
 
 /*add plate_number field to the users table*/
 ALTER TABLE users ADD COLUMN car_plate_number VARCHAR(10);
-ALTER TABLE users ADD COLUMN user_top_up DECIMAL(6,2);
 
 /*20180911 added by kskoh*/
 /* create location table */
 CREATE TABLE IF NOT EXISTS location(
-  trans_loc_id INT(11) AUTO_INCREMENT PRIMARY KEY,
+  id INT(11) AUTO_INCREMENT PRIMARY KEY,
   loc_name VARCHAR(255) NOT NULL,
   loc_council VARCHAR(255) NOT NULL,
   loc_state VARCHAR(255)
@@ -42,23 +41,16 @@ CREATE TABLE IF NOT EXISTS transaction(
   trans_starttime char(5),
   trans_end DATE,
   trans_endtime char(5),
-  trans_loc_id INT(11) NOT NULL,
+  trans_loc INT(11) NOT NULL,
   trans_amount DECIMAL(10,2)
 );
 
-/*create db*/
-CREATE DATABASE IF NOT EXISTS parkmelaka_admin;
-USE parkmelaka_admin;
+/*add user top up for top up request*/
+ALTER TABLE users ADD COLUMN user_top_up DECIMAL(6,2);
 
-/*create admin table*/
-CREATE TABLE IF NOT EXISTS admin(
-  id INT(11) AUTO_INCREMENT PRIMARY KEY,
-  admin_ID VARCHAR(255) NOT NULL,
-  admin_password VARCHAR(255) NOT NULL,
-  admin_name VARCHAR(255)
-);
+/*eye rolling*/
+ALTER TABLE location
+CHANGE COLUMN id trans_loc_id INT(11);
 
-/*insert dummy data into admin table*/
-DELETE FROM admin;
-INSERT INTO admin (admin_ID, admin_password, admin_name)
-VALUES ("1121100123", "123456", "KS Koh");
+ALTER TABLE transaction
+CHANGE COLUMN trans_loc trans_loc_id INT(11);
